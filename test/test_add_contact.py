508f9638525bbc4761041fec69c0aaa01bca25cc
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-07-27 00:43:09 +0200

    Class-helpers were created for both tests: test_add_group and test_add_contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
+from fixture.application import Application
 from model.contact import Contact
-from fixture.application_contacts import ApplicationContacts
 import pytest
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationContacts()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_contact(app):
-    app.login( username="admin", password="secret")
-    app.create_new_contact(Contact( firstname="dfdfgh", lastname="ttttt", address="fghghjhkkvb", home="333333333",
+    app.session.login( username="admin", password="secret")
+    app.contacts.create_new_contact(Contact( firstname="dfdfgh", lastname="ttttt", address="fghghjhkkvb", home="333333333",
                             mobile="333333", work="1111111111", fax="111111111111111111111", email="rttyru@rtrt",
                             email2="fgghjj", homepage="wwwfghgj"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact( firstname="", lastname="",
+    app.session.login(username="admin", password="secret")
+    app.contacts.create_new_contact(Contact( firstname="", lastname="",
                             address="", home="", mobile="", work="", fax="", email="",
                             email2="", homepage=""))
-    app.logout()
+    app.session.logout()
