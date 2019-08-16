eca520c955dbe9eb10def54f3f87258fd772b25c
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-08-16 23:57:24 +0200

    Tests for adding group and contact were parameterized.

--- model/contact.py
+++ model/contact.py
@@ -23,7 +23,9 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s;%s:%s:%s:%s;%s:%s:%s:%s;%s" % (self.id, self.firstname, self.lastname , self.address, self.homephone,
+                      self.mobile, self.workphone, self.fax, self.email,
+                      self.email2, self.email3, self.homepage, self.secondaryphone)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and (self.firstname is None or other.firstname is None or self.firstname == other.firstname) and (self.lastname is None or other.lastname is None or self.lastname == other.lastname)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
+import random
+import string
+
+import pytest
+
 from fixture.application import Application
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", address="", homephone="",
+                      mobile="", workphone="", fax="", email="",
+                      email2="", email3 ="", homepage="", secondaryphone="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), address=random_string("address", 20),
+            homephone=random_string("homephone", 15), mobile=random_string("mobile", 15), workphone=random_string("workphone", 15),
+            fax=random_string("fax", 15), email=random_string("email", 20), email2=random_string("email2", 20),
+            email3=random_string("email3", 20),
+            homepage=random_string("homepage", 15), secondaryphone=random_string("secondaryphone", 15)
+            )
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contacts.get_contacts_list()
-    contact = Contact(firstname="Modest", lastname="Ivanov", address="fghghjhkkvb", homephone="333333333",
-                      mobile="333333", workphone="1111111111", fax="111111111111111111111", email="rttyru@rtrt",
-                      email2="fgghjj", email3 ="4545@344.ru", homepage="wwwfghgj", secondaryphone="33333")
     app.contacts.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contacts.count_contacts()
     new_contacts = app.contacts.get_contacts_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_add_empty_contact(app):
-#     old_contacts = app.contacts.get_contacts_list()
-#     contact = Contact( firstname="", lastname="",
-#                             address="", home="", mobile="", work="", fax="", email="",
-#                             email2="", homepage="")
-#     app.contacts.create_new_contact(contact)
-#     new_contacts = app.contacts.get_contacts_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
