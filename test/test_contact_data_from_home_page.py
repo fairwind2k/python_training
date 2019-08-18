414a3488341ecdbf132eae180d2776894aefca9d
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-08-18 11:18:29 +0200

    Renewed. Test for comparison data for contact from home page and data from edit page was created.

---
+++ test/test_contact_data_from_home_page.py
@@ -0,0 +1,34 @@
+from random import randrange
+
+from model.contact import Contact
+import re
+
+
+def test_name_on_home_page(app):
+    all_contacts = app.contacts.get_contacts_list()
+    index = randrange(len(all_contacts))
+    contact_from_home_page = app.contacts.get_contacts_list()[index]
+    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.all_e_mails_from_home_page == merge_e_mails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobile, contact.workphone, contact.secondaryphone]))))
+
+
+def merge_e_mails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3]))))
