2fb33f3c276ff653d8182b4b9f91ccde8c07b1ee
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-08-30 00:02:37 +0200

    Loading test data for contacts and groups from json-file by function pytest_generate_tests was realized.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -8,26 +8,8 @@ from fixture.application import Application
 from model.contact import Contact
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="", homephone="",
-                      mobile="", workphone="", fax="", email="",
-                      email2="", email3 ="", homepage="", secondaryphone="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), address=random_string("address", 20),
-            homephone=random_string("homephone", 15), mobile=random_string("mobile", 15), workphone=random_string("workphone", 15),
-            fax=random_string("fax", 15), email=random_string("email", 20), email2=random_string("email2", 20),
-            email3=random_string("email3", 20),
-            homepage=random_string("homepage", 15), secondaryphone=random_string("secondaryphone", 15)
-            )
-    for i in range(3)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contacts.get_contacts_list()
     app.contacts.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contacts.count_contacts()
